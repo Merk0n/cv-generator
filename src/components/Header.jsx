9265d1765061bf60a42d5451546f9bae96bac4f5
merk0n <127611734+Merk0n@users.noreply.github.com>
2024-03-10 17:58:36 +0100

    added two buttons "clear" & "load example" & some styling header part

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,26 @@ import ghIcon from "../assets/github-icon.svg";
 export default function Header() {
   return (
     <>
-      <div className='header'>
-        <h1>cv generator</h1>
+      <div className='header-box'>
+        <div className='header'>
+          <h1>cv generator</h1>
 
-        <label className='switch'>
-          <input type='checkbox' />
-          <span className='slider'></span>
-        </label>
-        <a href='https://github.com/Merk0n/cv-generator'>
-          <div className='create-by'>
-            Created by
-            <div>Merk0n</div>
-            <img className='ghIcon' src={ghIcon} alt='github' />
+          <label className='switch'>
+            <input type='checkbox' />
+            <span className='slider'></span>
+          </label>
+
+          <div className='header-btn'>
+            <button style={{color:"#e03131", backgroundColor:"#ffe3e3"}}>Clear</button>
+            <button>Load example</button>
           </div>
-        </a>
+          </div>
+          <div className='create-by'>
+            Created by Merk0n
+            <a href='https://github.com/Merk0n/cv-generator'>
+              <img className='ghIcon' src={ghIcon} alt='github' />
+            </a>
+        </div>
       </div>
     </>
   );
