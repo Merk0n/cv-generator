cfcd5ac656b2ebea2cd5aecd01423b1ef1fdd078
merk0n <127611734+Merk0n@users.noreply.github.com>
2024-03-10 23:22:18 +0100

    Add sidebar navigation to CVForm component

--- src/components/CVForm.jsx
+++ src/components/CVForm.jsx
@@ -2,17 +2,25 @@ import FormMaker from "./FormMaker.jsx";
 import Header from "./Header.jsx";
 import "../styles/CVForm.css";
 import { FORM_DATA } from "./formData.js";
+import SideButton from "./SideButton.jsx";
 
 export default function CVForm() {
   return (
     <div className='cv-form'>
-      <Header />
-      <div className='form-label'>
-        <FormMaker {...FORM_DATA[0]} />
-        <FormMaker {...FORM_DATA[1]} />
-        <FormMaker {...FORM_DATA[2]} />
-        <FormMaker {...FORM_DATA[3]} />
-        <FormMaker {...FORM_DATA[4]} />
+      <nav className='sidebar'>
+        <SideButton>Content</SideButton>
+        <SideButton>Customize</SideButton>
+      </nav>
+
+      <div className='container-form'>
+        <Header />
+        <div className='form-label'>
+          <FormMaker {...FORM_DATA[0]} />
+          <FormMaker {...FORM_DATA[1]} />
+          <FormMaker {...FORM_DATA[2]} />
+          <FormMaker {...FORM_DATA[3]} />
+          <FormMaker {...FORM_DATA[4]} />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/SideButton.jsx
@@ -0,0 +1,5 @@
+export default function SideButton({ children}) {
+    return (
+        <button>{children}</button>
+    );
+    }
