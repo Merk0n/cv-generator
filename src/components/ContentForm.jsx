9f6fde9c2e4958f871c3346d0393dffaf74245ca
merk0n <127611734+Merk0n@users.noreply.github.com>
2024-03-11 13:57:42 +0100

    Refactor form rendering in ContentForm and CustomizeForm components

--- src/components/ContentForm.jsx
+++ src/components/ContentForm.jsx
@@ -1,14 +1,12 @@
-import FormMaker from './FormMaker'
-import { FORM_DATA } from './formData'
+import FormMaker from './FormMaker';
+import { FORM_DATA } from './formData';
 
 export default function ContentForm() {
-    return (
-        <div className='form-label'>
-          <FormMaker {...FORM_DATA.content[0]} />
-          <FormMaker {...FORM_DATA.content[1]} />
-          <FormMaker {...FORM_DATA.content[2]} />
-          <FormMaker {...FORM_DATA.content[3]} />
-          <FormMaker {...FORM_DATA.content[4]} />
-        </div>
-    )
+  return (
+    <div className='form-label'>
+      {FORM_DATA.content.map((contentItem) => (
+        <FormMaker key={contentItem.formTitle} {...contentItem} />
+      ))}
+    </div>
+  );
 }
--- src/components/CustomizeForm.jsx
+++ src/components/CustomizeForm.jsx
@@ -1,14 +1,12 @@
-import FormMaker from './FormMaker'
-import { FORM_DATA } from './formData'
+import FormMaker from './FormMaker';
+import { FORM_DATA } from './formData';
 
 export default function CustomizeForm() {
-    return (
-        <div className='form-label'>
-          <FormMaker {...FORM_DATA.customize[0]} />
-          <FormMaker {...FORM_DATA.customize[1]} />
-          <FormMaker {...FORM_DATA.customize[2]} />
-          <FormMaker {...FORM_DATA.customize[3]} />
-          <FormMaker {...FORM_DATA.customize[4]} />
-        </div>
-    )
+  return (
+    <div className='form-label'>
+      {FORM_DATA.customize.map((customizeItem) => (
+        <FormMaker key={customizeItem.formTitle} {...customizeItem} />
+      ))}
+    </div>
+  );
 }
