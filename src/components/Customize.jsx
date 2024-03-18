9c5b495a03e04f965021c597e8df4eec9d27854c
merk0n <127611734+Merk0n@users.noreply.github.com>
2024-03-18 19:19:19 +0100

    Add customization options to Customize component - not working

--- src/components/Customize.jsx
+++ src/components/Customize.jsx
@@ -1,7 +1,42 @@
+import '../styles/Customize.css';
 export default function Customize() {
   return (
-    <div>
-      <p>Customize your CV</p>
+    <div className='customize'>
+      <div className='customize-option'>
+        <h2>Layout</h2>
+        <div className='layout-options'>
+          <button>
+            <div className='custom-setting top-layout'></div>
+            TOP
+          </button>
+          <button>
+            <div className='custom-setting left-layout'></div>
+            LEFT
+          </button>
+        </div>
+      </div>
+
+      <div className='customize-option'>
+        <h2>Color</h2>
+        <div className='color-options'>
+          Accent Color:
+          <input className='rounded-input' type='color' value={'#0e374e'} />
+        </div>
+      </div>
+
+      <div className='customize-option'>
+        <h2>Font</h2>
+        <div className='font-options'>
+          <button className='roboto-font'>
+            <span className='custom-font'>Aa</span>
+            Roboto
+          </button>
+          <button className='arial-font'>
+            <span className='custom-font'>Aa</span>
+            Arial
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
